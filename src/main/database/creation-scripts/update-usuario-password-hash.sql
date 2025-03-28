151fcedb402e11a344b00eb1e3f2d7a76a81282b
Amanda Caetano Nasser <acn.psi23@gmail.com>
2025-03-28 17:49:45 -0300

    Alterações na pagina-inicial e incluído script SQL para hash de senha

---
+++ src/main/database/creation-scripts/update-usuario-password-hash.sql
@@ -0,0 +1,10 @@
+-- Comando para implementar o Hash para criptografia de senha (pode rodar tudo de uma vez só).
+-- Autora: Amanda Caetano Nasser
+-- Data: 28/03/2025
+-- Última alteração em: 28/03/2025
+
+USE TechSmartDB
+
+-- Altera a senha de todos os usuários pra "teste123"
+UPDATE Usuario SET senha = '4PaBNNKdwybRFd5Mj6uHAKPEsAI='
+GO
