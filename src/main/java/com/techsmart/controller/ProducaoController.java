99c9d60257c96db3031d04a839a4b81862ae213f
Gustavo Henrique <guzaguzahenrique@gmail.com>
2025-03-28 21:48:43 -0300

    Teste commit

--- src/main/java/com/techsmart/controller/ProducaoController.java
+++ src/main/java/com/techsmart/controller/ProducaoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/producao")
 public class ProducaoController {
-
+   
 	@GetMapping
 	public String carregarPagina(Model model) {
 		return "pages/cadastro-producao";
